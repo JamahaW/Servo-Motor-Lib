c9333698994c922539434198b4c105338c376c6b
JamahaW <153020210+JamahaW@users.noreply.github.com>
2024-12-16 21:50:36 +0300

    add PID.hpp

--- src/servomotor/core/PID.hpp
+++ src/servomotor/core/PID.hpp
@@ -3,90 +3,46 @@
 namespace servomotor {
     namespace core {
 
-        /// Интерфейс регулятора общего назначения
-        template<class T> struct Regulator {
-            /// Получить значение регулятора
-            virtual T calc(T error) = 0;
-        };
-
-        template<class T> struct Proportional : Regulator<T> {
-            const float kp;
-
-            T calc(T error) override { return error * this->kp; }
-        };
-
-        template<class TimeType> struct TimeHelper {
-            /// Единица измерения времени
+        /// ПИД-Регулятор
+        template<class T, class TimeType = uint32_t> class PID {
+        public:
             using TimeMs = TimeType;
 
-            TimeMs last_update{0};
+        private:
 
-            /// Получить дельту времени
-            TimeMs getDeltaMs() {
-                TimeMs ret = getCurrentTime() - this->last_update;
-                this->last_update = getCurrentTime();
-                return ret;
+            const float kp;
+            const float ki;
+            const float kd;
+
+            T last_error{0};
+            T accumulated_error{0};
+            TimeMs last_time{0};
+
+        public:
+
+            explicit PID(float kp, float ki, float kd) :
+                kp{kp}, ki{ki}, kd{kd} {}
+
+            /// Получить значение регулятора
+            T calc(T error) {
+                const TimeMs delta = getCurrentTime() - last_time;
+
+                T d_term{0};
+
+                if (delta > 0) {
+                    this->accumulated_error += error * delta;
+                    d_term = this->kd * (error - last_error) / delta;
+                }
+
+                last_error = error;
+                last_time = getCurrentTime();
+
+                return (error * kp) + (this->accumulated_error * ki) + d_term;
             }
 
         private:
+
             inline static TimeMs getCurrentTime() { return reinterpret_cast<TimeMs>(millis()); }
-        };
-
-        /// Интегральный регулятор
-        template<class T, class TimeType> struct Integral : Regulator<T> {
-
-            const float ki;
-
-            TimeHelper<TimeType> time_helper;
-            T accumulated_error{0};
-
-            explicit Integral(float ki) :
-                ki{ki} {}
-
-            T calc(T error) override {
-                auto delta_ms = this->time_helper.getDeltaMs();
-
-                if (delta_ms > 0) {
-                    this->accumulated_error += error * delta_ms;
-                }
-
-                return this->accumulated_error * this->ki;
-            }
-
-        };
-
-        /// Дифференциальный регулятор
-        template<class T, class TimeType> struct Derivative : Regulator<T> {
-
-            const float kd;
-
-            TimeHelper<TimeType> time_helper;
-            T last_error{0};
-
-            explicit Derivative(float kd) :
-                kd{kd} {}
-
-            T calc(T error) override {
-                auto delta_ms = this->time_helper.getDeltaMs();
-
-                T ret{0};
-
-                if (delta_ms > 0) {
-                    ret = this->kd * (this->last_error - error) / delta_ms;
-                }
-
-                return ret;
-            }
-
-        };
-
-        template<class T, class RegulatorA, class RegulatorB> struct PairRegulator : Regulator<T> {
-            RegulatorA regulator_a;
-            RegulatorB regulator_b;
-
-            T calc(T error) override {
-                return dynamic_cast<Regulator<T>>(this->regulator_b).calc(error) + dynamic_cast<Regulator<T>>(this->regulator_b).calc(error);
-            }
         };
 
     }
